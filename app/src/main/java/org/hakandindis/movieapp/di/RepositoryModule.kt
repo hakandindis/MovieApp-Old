71d40037f1c2eff3e9cbd73a0de3c281b6f87a4d
Hakan Dındış <hakandindis@gmail.com>
2023-02-16 23:25:42 +0300

    git initialized

---
+++ app/src/main/java/org/hakandindis/movieapp/di/RepositoryModule.kt
@@ -0,0 +1,10 @@
+package org.hakandindis.movieapp.di
+
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+
+@Module
+@InstallIn(SingletonComponent::class)
+class RepositoryModule {
+}
